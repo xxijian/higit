3c7a144ed3449ce216aeed4f773dd83efe0547ae
席剑 <xxijian@163.com>
2020-02-13 14:27:59 +0800

    idea测试git

---
+++ src/com/xijian/HiGit.java
@@ -0,0 +1,11 @@
+package com.xijian;
+
+/**
+ * @author XiJian
+ * @date 2020/2/13 - 14:26
+ */
+public class HiGit {
+    public static void main(String[] args) {
+        System.out.println("hi git");
+    }
+}
